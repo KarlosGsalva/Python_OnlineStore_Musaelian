ad1aa20a9c31d1879d54c8b6cede736cf9bca1b6
Karlen M <karlenmusaelian@gmail.com>
2024-06-19 22:07:56 +0300

    project of part_2

--- src/core/admin.py
+++ src/core/admin.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import User, Customer, Product, Stock, Cart, Order
+from .models import Customer, Product, Stock, Cart, Order, Category
 
-admin.site.register(User, UserAdmin)
 admin.site.register(Customer)
 admin.site.register(Product)
 admin.site.register(Stock)
 admin.site.register(Cart)
 admin.site.register(Order)
+admin.site.register(Category)
--- src/core/models.py
+++ src/core/models.py
@@ -1,9 +1,4 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-
-class User(AbstractUser):
-    pass
 
 
 class Customer(models.Model):
@@ -27,6 +22,7 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     image = models.ImageField(upload_to='products/')
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
 
     class Meta:
         db_table = "products"
@@ -79,3 +75,12 @@ class Order(models.Model):
 
     def __str__(self):
         return f"Order {self.id} by {self.customer.first_name}"
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        db_table = "categories"
+        verbose_name = "categories"
+        verbose_name_plural = "categories"
